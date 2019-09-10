0f3c0f0cb6dcb2314d6d92c669daf5c33ca812d3
maria t bowser <mariatbowser@mimi.local>
2019-09-10 12:56:12 -0400

    First commit

---
+++ newsdata.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+
+db = psycopg2.connect("dbname=news")
+
+cursor = db.cursor()
+
+cursor.execute( "select from authors where")
+
+results = cursor.fetchall()
+
+print results
+
+db.close()
+
+
