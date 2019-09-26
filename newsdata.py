fd56e938620c9ad127c64d6c703acab05285296e
vagrant <vagrant@vagrant.vm>
2019-09-26 20:54:50 +0000

    new changes

--- newsdata.py
+++ newsdata.py
@@ -1,15 +1,38 @@
 import psycopg2
 
-db = psycopg2.connect("dbname=news")
+query1 = ""
+query2 = ""
+query3 = ""
 
-cursor = db.cursor()
+#connecting to db
+def connect_to_db():
+    db = psycopg2.connect("dbname=news")
+    cursor = db.cursor()
+    #represents the instance of the db connection
+    return cursor
 
-cursor.execute( "select from authors where")
+#get 3 most popular articles    
+def question_one(query1):
+    pass
+#return name and article views
 
-results = cursor.fetchall()
-
-print results
-
-db.close()
+def question_two(query2):
+    pass
 
 
+def question_three(query3):
+    pass
+
+
+
+
+
+def main():
+    cur = connect_to_db()
+    #called connect to db, assinged cursor to cur
+    print(question_one())
+    print(question_two())
+    print(question_three())
+
+
+main()
